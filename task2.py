31d5238a872523314710cb8c43c27110b3e7833b
ronald-237 <75060387+ronald-237@users.noreply.github.com>
2020-11-26 11:29:43 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+a=b=c=40
+print(f"Divide a by 10:{a/10}\nDivide a by 10:{a//10}\nMultiply b by 50:{b*50}\nAdd c value by 60:{c+60}")
+string="Apple"
+print(string[:2]+"G"+string[3:])
+m=5
+n=6.00
+print(f"{int(n)}")
+print(f"{float(m)}")
