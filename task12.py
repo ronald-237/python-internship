96741511781d0036d21fc6cf5ae6bdeaa68b8180
ronald-237 <75060387+ronald-237@users.noreply.github.com>
2020-12-04 21:31:06 +0530

    Create task12.py

---
+++ task12.py
@@ -0,0 +1,19 @@
+import json
+#dictionary
+print(json.dumps({"Name":"Nelson","dosh":20}))
+#list
+print(json.dumps(["raj","Krish"]))
+#string
+print(json.dumps(("aruna","john")))
+#tuple
+print(json.dumps("ruby"))
+#integer
+print(json.dumps(80))
+#float
+print(json.dumps(25.1))
+#True
+print(json.dumps(True))
+#False
+print(json.dumps(False))
+#None
+print(json.dumps(None))
